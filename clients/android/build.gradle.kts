b6687761587e1fc55569ab6c2f90df7cfeaf659a
Harshil Waghela <harshilwaghela189@gmail.com>
2026-09-02 21:35:02 +0530

    Connect Android Gradle build to React Native

--- clients/android/build.gradle.kts
+++ clients/android/build.gradle.kts
@@ -2,4 +2,15 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.facebook.react") apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("../mobile/node_modules/react-native/android") }
+    }
+}
+
+apply(plugin = "com.facebook.react.rootproject")
