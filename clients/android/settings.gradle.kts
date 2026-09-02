826e652d6c0a4ee097d3235a80a38674471bc34f
Harshil Waghela <harshilwaghela189@gmail.com>
2026-09-02 21:34:58 +0530

    Route Android build into mobile client

--- clients/android/settings.gradle.kts
+++ clients/android/settings.gradle.kts
@@ -1,4 +1,24 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    includeBuild("../mobile/node_modules/@react-native/gradle-plugin")
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("../mobile/node_modules/react-native/android") }
+    }
+}
+
 rootProject.name = "CuraVeris"
 include(":app")
+include(":react-native-safe-area-context")
+project(":react-native-safe-area-context").projectDir = file("../mobile/node_modules/react-native-safe-area-context/android")
+include(":react-native-screens")
+project(":react-native-screens").projectDir = file("../mobile/node_modules/react-native-screens/android")
