a5174f3c4aa6f4778d85770690f6dad36d2e400c
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-03 04:04:05 +0530

    Configure React Native package registry in application class

--- clients/android/app/src/main/java/in/curaveris/app/ReactCuraVerisApplication.kt
+++ clients/android/app/src/main/java/in/curaveris/app/ReactCuraVerisApplication.kt
@@ -1,16 +1,22 @@
 package `in`.curaveris.app
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
+import com.swmansion.rnscreens.RNScreensPackage
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage
 
 class ReactCuraVerisApplication : Application(), ReactApplication {
     private val nativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
+        override fun getPackages(): List<ReactPackage> = listOf(
+            MainReactPackage(),
+            SafeAreaContextPackage(),
+            RNScreensPackage()
+        )
 
         override fun getJSMainModuleName(): String = "index"
 
