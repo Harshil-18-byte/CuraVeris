49a7720382b2e836df37fba87004f13eb6ba044b
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-04 14:48:25 +0530

    Fix Objective-C export macro syntax and add header guards in SimCardModule

--- clients/ios/CuraVeris/SimCardModule.m
+++ clients/ios/CuraVeris/SimCardModule.m
@@ -1,6 +1,11 @@
-#import "React/RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#endif
 
-@interface RCT_EXTERN_MODULE(SimCardModule, NSObject)
+#ifdef RCT_EXTERN_MODULE
+RCT_EXTERN_MODULE(SimCardModule, NSObject)
 
 RCT_EXTERN_METHOD(
   getSimCards:(RCTPromiseResolveBlock)resolve
@@ -8,3 +13,4 @@ RCT_EXTERN_METHOD(
 )
 
 @end
+#endif
