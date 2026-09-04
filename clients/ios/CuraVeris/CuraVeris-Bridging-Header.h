c8d569d9e644f29b21279862f3d8d90af01ad91c
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-04 14:45:16 +0530

    Add Objective-C bridging header for Swift React Native runtime

---
+++ clients/ios/CuraVeris/CuraVeris-Bridging-Header.h
@@ -0,0 +1,12 @@
+#ifndef CuraVeris_Bridging_Header_h
+#define CuraVeris_Bridging_Header_h
+
+// React Native headers
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTRootView.h>
+#import <React/RCTLog.h>
+#import <React/RCTUtils.h>
+
+#endif
