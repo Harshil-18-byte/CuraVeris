61a72908f71172dc60d3e9afa6d0adb73c423c02
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-05 23:53:59 +0530

    feat(mobile): add SimCardModule.m module to native mobile application

---
+++ mobile/ios/CuraVeris/SimCardModule.m
@@ -0,0 +1,16 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#endif
+
+#ifdef RCT_EXTERN_MODULE
+@interface RCT_EXTERN_MODULE(SimCardModule, NSObject)
+
+RCT_EXTERN_METHOD(
+  getSimCards:(RCTPromiseResolveBlock)resolve
+  rejecter:(RCTPromiseRejectBlock)reject
+)
+
+@end
+#endif
