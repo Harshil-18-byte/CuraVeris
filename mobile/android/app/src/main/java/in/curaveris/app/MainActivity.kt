1b28f3c8e2e96661c12cb5c992b68c290edaae45
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-05 23:53:32 +0530

    feat(mobile): add MainActivity.kt module to native mobile application

---
+++ mobile/android/app/src/main/java/in/curaveris/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package in.curaveris.app
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String = "CuraVeris"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
